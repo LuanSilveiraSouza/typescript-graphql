36050da6f6ae852e8a4a94bbecfb55ff0fe1174a
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2020-12-14 14:17:43 -0300

    feat: added category schema and resolver

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,15 @@ import 'reflect-metadata';
 import { buildSchema } from 'type-graphql';
 import { ApolloServer, SchemaDirectiveVisitor } from 'apollo-server';
 
+import CategoryResolver from './graphql/category/CategoryResolver';
 import './utils/connection';
 
 async function bootstrap() {
-	const schema = await buildSchema({ resolvers: [] });
+	const schema = await buildSchema({ resolvers: [CategoryResolver] });
 
-    const server = new ApolloServer({ schema });
-    
-    server.listen({ port: 3030 }, () => console.log('Listening');
-    )
+	const server = new ApolloServer({ schema });
+
+	server.listen({ port: 3030 }, () => console.log('Listening'));
 }
 
 bootstrap();
